5fb5982976bed593c01392c913362f711b32a0f4
Alejandro Reyes <agreyesaldeco@gmail.com>
2022-01-18 11:01:02 -0800

    project added
    SCENE LOADER CHALLENGE SUBMISSION ASSIGNMENT

---
+++ mini_solar_sys/Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SolarSystem : MonoBehaviour
+{
+
+    public void CreateSolarSystem() {
+        SceneManager.LoadScene(2);
+    }
+}
